ccedf1e28549d14894992dc2e7d72520282e4a99
thallgren <thomas@tada.se>
2012-04-27 06:28:25 +0200

    Initial commit

---
+++ src/main/java/org/cloudsmith/hammer/jenkins/StackValidatorBuilder.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2012 Cloudsmith Inc. and other contributors, as listed below.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Thomas Hallgren (Cloudsmith Inc.) - initial API and implementation
+ */
+package org.cloudsmith.hammer.jenkins;
+
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import hudson.tasks.Builder;
+
+import java.io.ByteArrayInputStream;
+import java.io.PrintStream;
+import java.net.URI;
+
+import org.apache.commons.codec.binary.Base64;
+import org.cloudsmith.hammer.api.model.Diagnostic;
+import org.cloudsmith.hammer.api.model.Provider;
+import org.cloudsmith.hammer.api.model.Repository;
+import org.cloudsmith.hammer.api.model.ResultWithDiagnostic;
+import org.cloudsmith.hammer.api.service.RepositoryService;
+import org.cloudsmith.hammer.api.service.StackHammerFactory;
+import org.cloudsmith.hammer.api.service.StackService;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+/**
+ * A {@link Builder} that performs Stack Hammer Validation
+ */
+public class StackValidatorBuilder extends Builder {
+
+	private final String branch;
+
+	private final String stackName;
+
+	private final String accessKey;
+
+	@DataBoundConstructor
+	public StackValidatorBuilder(String stackName, String branch, String accessKey) {
+		this.stackName = stackName;
+		this.branch = branch;
+		this.accessKey = accessKey;
+	}
+
+	public String getAccessKey() {
+		return accessKey;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	@Override
+	public StackValidatorDescriptor getDescriptor() {
+		return (StackValidatorDescriptor) super.getDescriptor();
+	}
+
+	public String getStackName() {
+		return stackName;
+	}
+
+	@Override
+	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+		try {
+			PrintStream logger = listener.getLogger();
+			String serverURL = getDescriptor().getServerURL();
+			if(serverURL == null)
+				serverURL = "https://stackhammer.cloudsmith.com:443/api";
+
+			URI uri = URI.create(serverURL);
+			logger.format(
+				"Using parameters%n scheme=%s%n host=%s%n port=%s%n prefix=%s%n token=%s%n", uri.getScheme(),
+				uri.getHost(), uri.getPort(), uri.getPath(), getAccessKey());
+
+			Injector injector = Guice.createInjector(new StackHammerModule(
+				uri.getScheme(), uri.getHost(), uri.getPort(), uri.getPath(), getAccessKey()));
+
+			StackHammerFactory factory = injector.getInstance(StackHammerFactory.class);
+			RepositoryService repoService = factory.createRepositoryService();
+			String[] splitName = getStackName().split("/");
+			String owner = splitName[0];
+			String name = splitName[1];
+			logger.format("Sending order to clone repository %s/%s to Stack Hammer%n", owner, name);
+			ResultWithDiagnostic<Repository> cloneResult = repoService.cloneRepository(
+				Provider.GITHUB, owner, name, branch);
+
+			if(cloneResult.getSeverity() == Diagnostic.ERROR) {
+				listener.error(cloneResult.toString());
+				return false;
+			}
+			logger.format("Clone of %s/%s was a success%n", owner, name);
+
+			StackService stackService = factory.createStackService();
+			Repository repo = cloneResult.getResult();
+			ResultWithDiagnostic<String> validationResult = stackService.validateStack(repo, repo.getOwner() + "/" +
+					repo.getName());
+
+			if(validationResult.getSeverity() == Diagnostic.ERROR) {
+				listener.error(validationResult.toString());
+				return false;
+			}
+
+			String svgStr = validationResult.getResult();
+			if(svgStr != null) {
+				FilePath ws = build.getWorkspace();
+				FilePath stackDir = ws.child(owner).child(name);
+				stackDir.mkdirs();
+				FilePath svgImg = stackDir.child("graph.svg");
+				svgImg.copyFrom(new ByteArrayInputStream(Base64.decodeBase64(svgStr)));
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace(listener.error("Exception during validation of %s", getStackName()));
+			return false;
+		}
+		return true;
+	}
+}
---
+++ src/main/java/org/cloudsmith/hammer/jenkins/StackValidatorDescriptor.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2012 Cloudsmith Inc. and other contributors, as listed below.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Thomas Hallgren (Cloudsmith Inc.) - initial API and implementation
+ */
+
+package org.cloudsmith.hammer.jenkins;
+
+import hudson.Extension;
+import hudson.model.AbstractProject;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import hudson.util.FormValidation;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.servlet.ServletException;
+
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * Descriptor for {@link StackValidatorBuilder}. Used as a singleton.
+ * The class is marked as public so that it can be accessed from views.
+ * 
+ * <p>
+ * See <tt>src/main/resources/com/cloudsmith/hammer/jenkins/StackValidatorBuilder/*.jelly</tt>
+ * for the actual HTML fragment for the configuration screen.
+ */
+@Extension
+public final class StackValidatorDescriptor extends BuildStepDescriptor<Builder> {
+	/**
+	 * To persist global configuration information,
+	 * simply store it in a field and call save().
+	 * 
+	 * <p>
+	 * If you don't want fields to be persisted, use <tt>transient</tt>.
+	 */
+	private String serverURL;
+
+	public StackValidatorDescriptor() {
+		super(StackValidatorBuilder.class);
+		load();
+	}
+
+	@Override
+	public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+		serverURL = formData.getString("serverURL");
+		save();
+		return super.configure(req, formData);
+	}
+
+	/**
+	 * Performs on-the-fly validation of the form field 'serverURL'.
+	 * 
+	 * @param value
+	 *        This parameter receives the value that the user has typed.
+	 * @return
+	 *         Indicates the outcome of the validation. This is sent to the browser.
+	 */
+	public FormValidation doCheckServerURL(@QueryParameter String value) throws IOException, ServletException {
+		if(value.length() == 0)
+			// This is OK, we'll use the default
+			return FormValidation.ok();
+
+		try {
+			URI uri = new URI(value);
+			String scheme = uri.getScheme();
+			if("http".equalsIgnoreCase(scheme) || "https".equalsIgnoreCase(scheme))
+				return FormValidation.ok();
+			return FormValidation.error("The Server URL must use http or https");
+		}
+		catch(URISyntaxException e) {
+			return FormValidation.error("The Server URL is not syntactially correct: %s", e.getMessage());
+		}
+	}
+
+	/**
+	 * Performs on-the-fly validation of the form field 'stackName'.
+	 * 
+	 * @param value
+	 *        This parameter receives the value that the user has typed.
+	 * @return
+	 *         Indicates the outcome of the validation. This is sent to the browser.
+	 */
+	public FormValidation doCheckStackName(@QueryParameter String value) throws IOException, ServletException {
+		if(value.length() == 0)
+			return FormValidation.error("Please set a stack name");
+		String[] split = value.trim().split("/");
+		if(split.length == 2) {
+			String owner = split[0].trim();
+			String name = split[1].trim();
+			if(!(owner.isEmpty() || name.isEmpty())) {
+				return FormValidation.ok();
+			}
+		}
+		return FormValidation.error("Stack name must be in the form <owner>/<name>");
+	}
+
+	/**
+	 * This human readable name is used in the configuration screen.
+	 */
+	@Override
+	public String getDisplayName() {
+		return "Stack Hammer Validator Configuration";
+	}
+
+	/**
+	 * This method returns the serverURL of the global configuration.
+	 */
+	public String getServerURL() {
+		return serverURL;
+	}
+
+	/**
+	 * Indicates that this builder can be used with all kinds of project types
+	 */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+		return true;
+	}
+}
